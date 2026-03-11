0dcd48c31e93a3254358e4273672b675b89f54a4
superpapryka <emilprajs@gmail.com>
2026-03-11 11:03:48 +0100

    Add files via upload

---
+++ AnswerPicker/Services/AbsenceStatusConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace AnswerPicker.Services
+{
+    public class AbsenceStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isAbsent)
+            {
+                return isAbsent ? "Present" : "Absent";
+            }
+            return "Absent";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
