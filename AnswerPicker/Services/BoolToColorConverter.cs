6bb410c54d5d61458044205d1e623cc565dd2ac8
superpapryka <emilprajs@gmail.com>
2026-03-11 10:13:34 +0100

    Add files via upload

---
+++ AnswerPicker/Services/BoolToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace AnswerPicker.Services
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isLucky)
+            {
+                return isLucky ? Colors.Green : Colors.Transparent;
+            }
+            return Colors.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
