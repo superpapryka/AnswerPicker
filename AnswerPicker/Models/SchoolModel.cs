96445b51576723ea605f5cb492d5778c997b67ab
superpapryka <emilprajs@gmail.com>
2026-03-11 11:56:29 +0100

    Add files via upload

---
+++ AnswerPicker/Models/SchoolModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AnswerPicker.Models
+{
+    public class SchoolModel
+    {
+        public List<ClassModel> Classes { get; set; } = new List<ClassModel>();
+
+        public SchoolModel() { }
+
+        public void AddClass(ClassModel classModel)
+        {
+            if (!Classes.Exists(c => c.Name == classModel.Name))
+                Classes.Add(classModel);
+        }
+
+        public void RemoveClass(string className)
+        {
+            var existing = Classes.Find(c => c.Name == className);
+            if (existing != null) Classes.Remove(existing);
+        }
+
+        public ClassModel? GetClass(string className)
+        {
+            return Classes.Find(c => c.Name == className);
+        }
+    }
+}
