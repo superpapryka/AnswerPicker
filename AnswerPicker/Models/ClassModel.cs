10fdf8acacf664359298bd825e4e123e7e4b9921
superpapryka <emilprajs@gmail.com>
2026-03-11 11:55:54 +0100

    Update ClassModel.cs

--- AnswerPicker/Models/ClassModel.cs
+++ AnswerPicker/Models/ClassModel.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
-namespace AnswerPicker.Models;
-
-public class ClassModel
+namespace AnswerPicker.Models
 {
-    public string Name { get; set; } = string.Empty;
-    public List<Student> Students { get; set; } = new List<Student>();
+    public class ClassModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        public ClassModel() { }
+        public ClassModel(string name) { Name = name; }
+    }
 }
